a602fceb607f6c4c888d39dc5546edb17ae6dcf6
Anupam Sanidhya <sanidhyaanupam@gmail.com>
2021-01-30 00:29:08 +0530

    First Commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Anupam Sanidhya",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Anupam Sanidhya and I'm a Software Engineer!",
+  githubUrl: "https://github.com/iridescent-anupam",
+  linkedinUrl: "https://www.linkedin.com/in/anupamsanidhya/",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
